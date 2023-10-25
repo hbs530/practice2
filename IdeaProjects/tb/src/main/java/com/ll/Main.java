7dd4e68931d50431db9b13ef0e0f3ef5216ab1f8
Hyun BeomSeok <hbs530@naver.com>
2023-10-25 17:41:23 +0900

    App 클래스 추가

---
+++ IdeaProjects/tb/src/main/java/com/ll/Main.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+public class Main {
+    public static void main(String[] args) {
+        new App().run();
+    }
+}
+
+class App{
+    void run(){
+        System.out.println("프로그램 실행");
+    }
+}
